89916d5a8857b18ca44a47ee6324c04e0bf446db
Veli Bacık <velibacik60@gmail.com>
2018-09-05 04:33:30 +0300

    xamarin forms mvvm and design complate

---
+++ RockPaper/RockPaper/View/GamePage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using RockPaper.ViewModel;
+using Xamarin.Forms;
+
+namespace RockPaper.View
+{
+    public partial class GamePage : ContentPage
+    {
+        GameViewModel gameViewModel;
+        public GamePage()
+        {
+            InitializeComponent();
+            BindingContext = gameViewModel = new GameViewModel(this);
+        }
+    }
+}
